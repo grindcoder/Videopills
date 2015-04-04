323a3f6fdda7feccc23f131a8b1b69187b8a2eab
Ursino Gabriele <ursinogabriele.0@gmail.com>
2015-04-04 12:43:40 +0200

    scritte le didascalie sotto la barra di ricerca e l'about,giusto per standardizzare la facciata della home

--- wsgi/video_manager/views.py
+++ wsgi/video_manager/views.py
@@ -11,11 +11,12 @@ def home(request):
     # TODO gabri
     # Non prendere tutti i video pills ma solo gli ultimi 6 che sono stati inseriti
     # in ordine temporale
-    pills= VideoContainer.objects.all().order_by('pub_date')[:6]
+    pills= VideoContainer.objects.all().order_by('-pub_date')[:6]
     return render_to_response('home/home.html', {"list_of_pills":pills} )
 
 def redirect_to_videopills():
     return "fuck"
+
 
 
 
