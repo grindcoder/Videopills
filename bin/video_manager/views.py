d579bf251e1b4765f13e323f158dc8d618c522f0
Ursino Gabriele <ursinogabriele.0@gmail.com>
2015-04-27 22:55:23 +0200

    Fixata la pagina di registrazione,che inizialmente non registrava i nuovi utenti.
    A registrazione avvenuta l'utente verrà ridirezionato alla pagina di login,con un messaggio che lo informa che il suo account verrà confermato

--- bin/video_manager/views.py
+++ bin/video_manager/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+
 # Create your views here.
 
 @login_required(login_url="/login")
@@ -99,34 +100,37 @@ def register(request):
     registered = False
     errors = None
 
- #   merge da home a develop in develop branch login(origin login)
+
     if request.method == 'POST':
         # Attempt to grab information from the raw form information.
         # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
+        #profile_form = UserProfileForm(data=request.POST)
 
         # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid() :#and profile_form.is_valid():
         # Save the user's form data to the database.
 
             user = user_form.save()
             user.set_password(user.password)
             user.save()
 
-            profile = profile_form.save()
-            profile.user = user
+            #profile = profile_form.save()
+            #profile.user = user
 
-            profile.save()
+            #profile.save()
 
             # Update our variable to tell the template registration was successful.
             registered = True
+
+            #return HttpResponseRedirect('/home/')
 
         # Invalid form or forms - mistakes or something else?
         # Print problems to the terminal.
         # They'll also be shown to the user.
         else:
             errors = user_form.errors
+
 
     # Not a HTTP POST, so we render our form using two ModelForm instances.
     # These forms will be blank, ready for user input.
