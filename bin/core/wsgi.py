510c1c578c39501612c1dd42bbb8e627d6b76566
Simone Chiorazzo <chiora93@gmail.com>
2015-04-27 20:36:21 +0200

    Eliminato bin/application Si utilizzerà il bin/core/wsgi.py per integrarsi con un webserver
    Aggiunto shebang del virtualenv python3.1 raspberry

--- bin/core/wsgi.py
+++ bin/core/wsgi.py
@@ -1,3 +1,4 @@
+#!/home/pi/.virtualenvs/env/bin/python
 # -*- coding: utf-8 -*-
 """
 WSGI config for core project.
@@ -14,19 +15,22 @@ import sys
 sys.path.append('/var/www/videopills/wsgi')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-if 'OPENSHIFT_REPO_DIR' in os.environ:
-    sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'core'))
-    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-    os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
-    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-    try:
-        with open(virtualenv, 'rb') as exec_file:
-            file_contents = exec_file.read()
-        compiled_code = compile(file_contents, virtualenv, 'exec')
-        exec_namespace = dict(__file__=virtualenv)
-        exec(compiled_code, exec_namespace)
-    except IOError:
-        pass
+
+## VECCHIA GESTIONE
+## TODO: DA RIVEDERE
+# if 'OPENSHIFT_REPO_DIR' in os.environ:
+# sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'core'))
+#     virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+#     os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
+#     virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+#     try:
+#         with open(virtualenv, 'rb') as exec_file:
+#             file_contents = exec_file.read()
+#         compiled_code = compile(file_contents, virtualenv, 'exec')
+#         exec_namespace = dict(__file__=virtualenv)
+#         exec(compiled_code, exec_namespace)
+#     except IOError:
+#         pass
 
 
 from django.core.wsgi import get_wsgi_application
